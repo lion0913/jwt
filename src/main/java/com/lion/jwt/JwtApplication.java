45f27f6fe8c1a10371ba9f0136387a051899018c
박다정 <ekwjd990913@gmail.com>
2022-10-05 13:20:33 +0900

    accessToken생성 및 인증, claims 얻기

--- src/main/java/com/lion/jwt/JwtApplication.java
+++ src/main/java/com/lion/jwt/JwtApplication.java
@@ -3,6 +3,7 @@ package com.lion.jwt;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JwtApplication {
@@ -11,4 +12,8 @@ public class JwtApplication {
         SpringApplication.run(JwtApplication.class, args);
     }
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
--- src/main/java/com/lion/jwt/app/jwt/JwtProvider.java
+++ src/main/java/com/lion/jwt/app/jwt/JwtProvider.java
@@ -1,5 +1,8 @@
 package com.lion.jwt.app.jwt;
 
+import com.lion.jwt.app.util.Util;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
@@ -8,6 +11,8 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
 
 @Component
 @EnableCaching
@@ -22,4 +27,40 @@ public class JwtProvider {
 
         return secretKey;
     }
+
+    public String generateAccessToken(Map<String, Object> claims, int seconds) {
+        long now = new Date().getTime();
+        Date accessTokenExpiresIn = new Date(now + 1000L * seconds);
+
+        return Jwts.builder()
+                .claim("body", Util.json.toStr(claims))
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public boolean verify(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSecretKey())
+                    .build()
+                    .parseClaimsJws(token);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public Map<String, Object> getClaims(String token) {
+        String body = Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("body", String.class);
+
+        return Util.json.toMap(body);
+    }
+
 }
